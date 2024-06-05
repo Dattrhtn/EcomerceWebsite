f8f29a8d8d585eb51207ab49df141a352e7a39b2
Dattrhtn <dattrinh9365@gmail.com>
2024-06-05 09:50:18 +0700

    update database

--- TMDT_1.sql
+++ TMDT_1.sql
@@ -1,33 +1,32 @@
 --use master
 --drop database DBBanHangTMDT
 --go
+
+
+
 create database DBBanHangTMDT
 go
 use DbBanHangTMDT
 go
-create table customer(
- customer_id int primary key,
+create table account(
+ account_id int primary key identity,
  first_name varchar(100),
  last_name varchar(100),
  email varchar(100),
  password varchar(100),
  address varchar(100),
+ [role] int,
  phone_number varchar(100),
 )
 go
-alter table customer
-add [role] int
-go
-EXEC sp_rename 'customer', 'Account'
-go
 
 create table Category (
- category_id int primary key,
+ category_id int primary key identity,
  name varchar(100),
 )
 go
 create table Product (
- product_id int primary key,
+ product_id int primary key identity,
  SKU varchar(100),
  description varchar(100),
  price decimal(10,2),
@@ -39,39 +38,39 @@ create table Product (
 
 go
 create table Payment(
- payment_id int primary key,
+ payment_id int primary key identity,
  payment_date datetime,
  payment_method varchar(100),
  amount decimal(10,2),
- customer_customer_id int,
- constraint payment_customer foreign key (customer_customer_id) references customer(customer_id)
+ account_account_id int,
+ constraint payment_account foreign key (account_account_id) references account(account_id)
 )
 create table shipment (
- shipment_id int primary key,
+ shipment_id int primary key identity,
  shipment_date datetime,
  address varchar(100),
  city varchar(100),
  state varchar(20),
  country varchar(50),
  zip_code varchar(10),
- customer_customer_id int,
- constraint shipment_customer foreign key (customer_customer_id) references customer(customer_id)
+ account_account_id int,
+ constraint shipment_account foreign key (account_account_id) references account(account_id)
 )
 go
 create table [Order](
- order_id int primary key,
+ order_id int primary key identity,
  order_date datetime,
  total_price decimal(10,2),
- Customer_customer_id int,
+ account_account_id int,
  Payment_payment_id int,
  Shipment_shipment_id int,
- constraint order_customer foreign key (Customer_customer_id) references customer(customer_id),
+ constraint order_account foreign key (account_account_id) references account(account_id),
  constraint order_payment foreign key (payment_payment_id) references Payment(payment_id),
  constraint order_shipment foreign key (Shipment_shipment_id) references shipment(shipment_id)
 )
 go
 create table order_item(
- order_item_id int,
+ order_item_id int identity,
  quantity int,
  price decimal(10,2),
  product_product_id int,
@@ -84,19 +83,19 @@ go
 create table cart (
  cart_id int,
  quantity int,
- customer_customer_id int,
+ account_account_id int,
  product_product_id int,
- primary key (cart_id, customer_customer_id),
- constraint cart_customer foreign key (Customer_customer_id) references customer(customer_id),
+ primary key (cart_id, account_account_id),
+ constraint cart_account foreign key (account_account_id) references account(account_id),
  constraint cart_product foreign key (product_product_id) references product(product_id),
 )
 go
 create table wishlist(
- wishlist_id int,
- Customer_customer_id int,
+ wishlist_id int identity,
+ account_account_id int,
  product_product_id int,
- primary key (wishlist_id, Customer_customer_id),
-  constraint wishlist_customer foreign key (Customer_customer_id) references customer(customer_id),
+ primary key (wishlist_id, account_account_id),
+  constraint wishlist_account foreign key (account_account_id) references account(account_id),
    constraint wishlist_product foreign key (product_product_id) references product(product_id),
 )
 
