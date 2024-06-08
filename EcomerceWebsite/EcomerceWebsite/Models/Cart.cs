111e333ff01068d2924b9f2cb83808a6d31c8dc1
trengzi <trangnguyen16022003@gmail.com>
2024-06-09 01:08:36 +0700

    commit

--- EcomerceWebsite/EcomerceWebsite/Models/Cart.cs
+++ EcomerceWebsite/EcomerceWebsite/Models/Cart.cs
@@ -10,15 +10,11 @@ namespace EcomerceWebsite.Models
     public partial class cart
     {
         [Key]
-        [Column(Order = 0)]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int cart_id { get; set; }
 
-        public int? quantity { get; set; }
+        public int? quantity { get; set; }S
 
-        [Key]
-        [Column(Order = 1)]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int account_account_id { get; set; }
 
         public int? product_product_id { get; set; }
