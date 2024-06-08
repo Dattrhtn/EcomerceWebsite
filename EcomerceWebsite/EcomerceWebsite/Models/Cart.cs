ec31ebe7e2875a0764fc0430b30984e002bcb2cc
Dinh Khac Dien <dinhkhacdien03@gmail.com>
2024-06-08 22:45:11 +0700

    finish chekout

--- EcomerceWebsite/EcomerceWebsite/Models/Cart.cs
+++ EcomerceWebsite/EcomerceWebsite/Models/Cart.cs
@@ -14,9 +14,6 @@ namespace EcomerceWebsite.Models
 
         public int? quantity { get; set; }
 
-        [Key]
-        [Column(Order = 1)]
-        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int account_account_id { get; set; }
 
         public int? product_product_id { get; set; }
